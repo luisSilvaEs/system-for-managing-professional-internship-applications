a559714596a5e7907aa8527f50656e2103a1bf1d
Luis Silva <siel_alb@hotmail.com>
2024-09-03 19:24:56 -0600

    Move error messages below each field. Create wrapper to move down error labels

--- app/components/forms/student/StudentForm.tsx
+++ app/components/forms/student/StudentForm.tsx
@@ -41,8 +41,10 @@ const StudentForm = () => {
     >
       <div className="b-form-wrapper">
         <div className="b-form-group b-form-group--borderless b-form__radio-buttons--horizontal">
-          <AutoField name="opcionElegida" />
-          <ErrorField name="opcionElegida" />
+          <div className="b-form__error-wrapper">
+            <AutoField name="opcionElegida" />
+            <ErrorField name="opcionElegida" />
+          </div>
         </div>
         <div className="b-form-group b-form-group--horizontal">
           <AutoField name="periodoProyectado" />
@@ -52,8 +54,10 @@ const StudentForm = () => {
           <h3>Datos del(a) residente</h3>
           <div className="b-form-group b-form-group--horizontal b-form-group--borderless">
             <h4>Nombre completo:</h4>
-            <AutoField name="nombre" />
-            <ErrorField name="nombre" />
+            <div className="b-form__error-wrapper">
+              <AutoField name="nombre" />
+              <ErrorField name="nombre" />
+            </div>
             <AutoField name="apellidoPaterno" />
             <AutoField name="apellidoMaterno" />
           </div>
@@ -75,25 +79,29 @@ const StudentForm = () => {
           </div>
           <div className="b-form-group b-form-group--horizontal b-form-group--borderless">
             <AutoField name="telefonoOcelular" />
-            <AutoField name="email" />
-            <ErrorField name="email" />
+            <div className="b-form__error-wrapper">
+              <AutoField name="email" />
+              <ErrorField name="email" />
+            </div>
           </div>
         </div>
         <div className="b-form-group">
           <h3>Datos de la empresa</h3>
           <AutoField name="nombreEmpresa" />
           <div className="b-form-group b-form-group--borderless b-form__radio-buttons--horizontal">
-            <RadioField
-              name="giroRamoSector"
-              options={[
-                { label: "Industrial", value: "Industrial" },
-                { label: "Servicios", value: "Servicios" },
-                { label: "Público", value: "Público" },
-                { label: "Privado", value: "Privado" },
-                { label: "Otro", value: "Otro" },
-              ]}
-            />
-            <ErrorField name="giroRamoSector" />
+            <div className="b-form__error-wrapper">
+              <RadioField
+                name="giroRamoSector"
+                options={[
+                  { label: "Industrial", value: "Industrial" },
+                  { label: "Servicios", value: "Servicios" },
+                  { label: "Público", value: "Público" },
+                  { label: "Privado", value: "Privado" },
+                  { label: "Otro", value: "Otro" },
+                ]}
+              />
+              <ErrorField name="giroRamoSector" />
+            </div>
             <DisplayIf<FormData>
               condition={(context) => context.model.giroRamoSector === "Otro"}
             >

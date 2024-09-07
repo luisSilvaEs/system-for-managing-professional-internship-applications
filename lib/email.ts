bed40c67b3bbce4138c978d85d5eac6344c02f10
Luis Silva <siel_alb@hotmail.com>
2024-09-06 19:29:11 -0600

    Fix email credentials to sent data, re build to run locally email sending

--- lib/email.ts
+++ lib/email.ts
@@ -31,16 +31,16 @@ export const sendConfirmationEmail = async ({
 }: EmailData): Promise<void> => {
   // Create a transporter using environment variables
   const transporter = nodemailer.createTransport({
-    service: "hotmail", // Use your preferred email service
+    service: "gmail", // Use your preferred email service
     auth: {
       user: process.env.EMAIL_USER, // Ensure these environment variables are set in your .env.local file
-      pass: process.env.EMAIL_PASS,
+      pass: process.env.EMAIL_APP_PASS,
     },
   });
 
   const mailOptions = {
     from: process.env.EMAIL_USER, // Sender address from environment variables
-    to: "siel.alb@gmail.com", // Replace with your destination email address
+    to: "siel_alb@hotmail.com", // Replace with your destination email address
     subject: `New Application from ${nombre}`,
     text: `You have received a new application.\n\nName: ${nombre}\nEmail: ${email}\nMessage: ${nombreEmpresa}`,
   };

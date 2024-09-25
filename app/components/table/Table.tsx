3e043aec02d68626898b181af93d66b327aefb25
Luis Silva <siel_alb@hotmail.com>
2024-09-24 20:18:30 -0600

    Use router in table to redirect to studentDetail page

--- app/components/table/Table.tsx
+++ app/components/table/Table.tsx
@@ -1,3 +1,6 @@
+"use client"; // Ensure the component is client-side
+
+import { useRouter } from "next/navigation"; // Directly import useRouter
 import { useState } from "react";
 import { CleanedItem } from "@/types/student";
 
@@ -14,8 +17,12 @@ const Table = ({ list, rowsPerPage = 2 }: TableProps) => {
   const indexOfFirstRow = indexOfLastRow - rowsPerPage;
   const currentData = list.slice(indexOfFirstRow, indexOfLastRow);
 
-  // Change page
   const paginate = (pageNumber: number) => setCurrentPage(pageNumber);
+
+  const router = useRouter();
+  const handleRowClick = (numeroControl: string) => {
+    router.push(`/private/queries/studentDetail/${numeroControl}`); // Navigate to the dynamic page
+  };
 
   return (
     <div className="container mx-auto">
@@ -36,9 +43,10 @@ const Table = ({ list, rowsPerPage = 2 }: TableProps) => {
           {currentData.map((item: CleanedItem) => (
             <tr
               key={item.id}
-              className="border-b border-gray-200 hover:bg-gray-100"
+              className="border-b border-gray-200 hover:bg-gray-100 cursor-pointer"
+              onClick={() => handleRowClick(item.numeroControl as string)}
             >
-              <td className="py-3 px-6 text-left whitespace-nowrap">
+              <td className="border-b border-gray-200 hover:bg-gray-100">
                 {item.numeroControl}
               </td>
               <td className="py-3 px-6 text-left">{item.nombre}</td>
@@ -54,18 +62,20 @@ const Table = ({ list, rowsPerPage = 2 }: TableProps) => {
       </table>
       <div className="flex justify-between items-center my-4">
         <button
+          aria-label="Pagina previa"
           className={`px-4 py-2 text-white bg-blue-500 rounded hover:bg-blue-700 ${
             currentPage === 1 && "opacity-50 cursor-not-allowed"
           }`}
           onClick={() => paginate(currentPage - 1)}
           disabled={currentPage === 1}
         >
-          Previo
+          Pag. previa
         </button>
         <div>
           Página {currentPage} de {totalPages}
         </div>
         <button
+          aria-label="Siguiente pagina"
           className={`px-4 py-2 text-white bg-blue-500 rounded hover:bg-blue-700 ${
             currentPage === totalPages && "opacity-50 cursor-not-allowed"
           }`}

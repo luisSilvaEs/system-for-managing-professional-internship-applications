062f9e3bc8f28125d3124866227440efba035894
Luis Silva <siel_alb@hotmail.com>
2024-09-24 20:15:16 -0600

    Minor fixes format

--- app/page.tsx
+++ app/page.tsx
@@ -1,17 +1,7 @@
-import StudentForm from "./components/forms/student/StudentForm";
+import React from "react";
 
-export default function Home() {
-  return (
-    <div>
-      <StudentForm
-        title="Solicitud de residencia profesional"
-        instructions={
-          <p>
-            Favor de llenar <strong style={{ color: "#db2828" }}>TODOS</strong>{" "}
-            los campos para procesar la solicitud.
-          </p>
-        }
-      />
-    </div>
-  );
-}
+const HomePage = () => {
+  return <div>Home Page</div>;
+};
+
+export default HomePage;

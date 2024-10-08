a17f4f6092697c79c9304fd609018d94c4b84ebc
Luis Silva <siel_alb@hotmail.com>
2024-10-08 17:10:34 -0600

    Make dynamic import from argon2

--- security/passwords.ts
+++ security/passwords.ts
@@ -1,20 +1,16 @@
-import argon2 from 'argon2';
-
-export async function hashPassword(password: string): Promise<string> {
+export async function hashPassword(plainPassword: string) {
   try {
-    return await argon2.hash(password, {
-      type: argon2.argon2id, // Ensure you're using the same type (argon2id in this case)
-    });
+    const argon2 = await import('argon2');
+    const hashedPassword = await argon2.hash(plainPassword);
+    return hashedPassword;
   } catch (error) {
-    console.error('Error hashing password:', error);
-    throw new Error('Failed to hash password');
+    throw new Error('Error hashing password');
   }
 }
 
-export async function verifyPassword(plainPassword: string, hashedPassword: string): Promise<boolean> {
+export async function verifyPassword(plainPassword: string, hashedPassword: string) {
   try {
-    console.log(`Plain password: ${plainPassword}`);
-    console.log(`Hashed password: ${ hashedPassword}`);
+    const argon2 = await import('argon2');
     return await argon2.verify(hashedPassword, plainPassword);
   } catch (error) {
     throw new Error('Error verifying password');

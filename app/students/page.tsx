2b74e5824fef90f99f3fff8a624dc841b3e7590b
Luis Silva <siel_alb@hotmail.com>
2024-10-09 14:56:46 -0600

    Add instructions to student form

--- app/students/page.tsx
+++ app/students/page.tsx
@@ -6,10 +6,47 @@ export default function Home() {
       <StudentForm
         title="Solicitud de residencia profesional"
         instructions={
-          <p>
-            Favor de llenar <strong style={{ color: "#db2828" }}>TODOS</strong>{" "}
-            los campos para procesar la solicitud.
-          </p>
+          <>
+            <p>
+              Llenar y enviar el siguiente formulario para procesar la solicitud
+              de residencia para el periodo proyectado.
+            </p>
+            <p>
+              <i>
+                <b>Indicaciones:</b>
+                <br />
+              </i>
+              <li>
+                Los campos marcados con{""}
+                <span
+                  style={{
+                    margin: "-.2em 0 0 .2em",
+                    content: "*",
+                    color: "#db2828",
+                  }}
+                >
+                  *
+                </span>{" "}
+                son obligatorios
+              </li>
+              <li>
+                Solo se permiten residencias de mínimo 1 integrantes y máximo 3
+              </li>
+              <li>
+                En caso de que el domicilio partircular o de la empresa no
+                cuente con <i>Número exterior</i>, indicar agregando <i>N/A</i>
+              </li>
+              <li>
+                Los campos <i>Número interior</i> son opcionales y pueden
+                permanecer en blanco
+              </li>
+              <li>
+                Revise bien los datos antes de dar click en el botón de enviar
+                ya que cualquier cambio deberá solicitarse al director de
+                residencias
+              </li>
+            </p>
+          </>
         }
       />
     </div>

9604b390213899baf9ea41ffc44449bd72ef59e8
Luis Silva <siel_alb@hotmail.com>
2024-10-18 15:48:34 -0600

    Add Skeleton to Card component. This Skeleton is shown while the info is loaded

--- app/components/card/Card.tsx
+++ app/components/card/Card.tsx
@@ -5,10 +5,11 @@ interface Spec {
 
 interface CardProp {
   header: string;
-  data: Spec[];
+  data: Spec[] | null; // Allow null for the loading state
 }
 
 const Card = ({ header, data }: CardProp) => {
+  const isLoading = !data || data.length === 0; // Determine loading state
   return (
     <div className="rounded-lg border bg-card text-card-foreground shadow-sm">
       <div className="flex flex-col space-y-1.5 p-6">
@@ -16,14 +17,21 @@ const Card = ({ header, data }: CardProp) => {
           {header}
         </h3>
         <div className="p-6 grid gap-4 sm:grid-cols-2 lg:grid-cols-3">
-          {data.map((item, index) => (
-            <div key={index + 1}>
-              <label className="text-sm font-medium leading-none peer-disabled:cursor-not-allowed peer-disabled:opacity-70">
-                {item.label}
-              </label>
-              <p>{item.paragraph}</p>
-            </div>
-          ))}
+          {isLoading
+            ? Array.from({ length: 6 }).map((_, index) => (
+                <div key={index} className="animate-pulse">
+                  <div className="h-4 bg-gray-300 rounded w-3/4 mb-2"></div>
+                  <div className="h-3 bg-gray-300 rounded w-full"></div>
+                </div>
+              ))
+            : data.map((item, index) => (
+                <div key={index + 1}>
+                  <label className="text-sm font-medium leading-none peer-disabled:cursor-not-allowed peer-disabled:opacity-70">
+                    {item.label}
+                  </label>
+                  <p>{item.paragraph}</p>
+                </div>
+              ))}
         </div>
       </div>
     </div>

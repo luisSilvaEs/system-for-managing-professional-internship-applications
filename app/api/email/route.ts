269c922beb6f961e97b33fe26f50b37f7f36b839
Luis Silva <siel_alb@hotmail.com>
2024-12-08 22:21:10 -0600

    Add validation to secure applications are sent just one time per student

--- app/api/email/route.ts
+++ app/api/email/route.ts
@@ -2,13 +2,13 @@
 import { NextApiRequest, NextApiResponse } from 'next';
 import { NextResponse } from 'next/server';
 import { sendEmail, createAttachment } from '@/lib/email';
-import { saveToDynamoDB } from '@/lib/dynamodb';
+import { saveToDynamoDB, isStudentUnique } from '@/lib/dynamodb';
 
 export async function POST(request: Request) {
   //console.log("SES_ACCESS_KEY_ID from environment:", process.env.SES_ACCESS_KEY_ID); //Added to debug on Amplify
   try {
     const data = await request.json();
-    const { emailResidente } = data;
+    const { emailResidente, numeroControl } = data;
 
     if (!emailResidente || !/\S+@\S+\.\S+/.test(emailResidente)) {
       console.error("Invalid email address:", emailResidente);
@@ -16,11 +16,16 @@ export async function POST(request: Request) {
     }
 
     //console.log("You got to /email/route.ts file");//Added to debug on Amplify
-    const attachmentPDF = await createAttachment( data );
-    await sendEmail(data, attachmentPDF);
-    await saveToDynamoDB(data);
-    
-    return NextResponse.json({ message: 'Email sent successfully' }, { status: 200 });
+
+    const isUnique = await isStudentUnique(numeroControl);
+    if ( isUnique ) {
+      const attachmentPDF = await createAttachment( data );
+      await sendEmail(data, attachmentPDF);
+      await saveToDynamoDB(data);
+      return NextResponse.json({ message: 'Data sent and stored successfully' }, { status: 200 });
+    } 
+    return NextResponse.json({ message: 'Data no sent nor stored. There is already a student who submitted this request before' }, { status: 200 });
+
   } catch (error) {
     console.error("Error sending email:", error);
     return NextResponse.json({ message: 'Failed to send email' }, { status: 500 });
